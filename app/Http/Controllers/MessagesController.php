73d7e99441f69a3b809c7d908d2372a1dfae3387
mijael <mijaelsaban@gmail.com>
2022-08-08 06:27:26 +0200

    Initial commit

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MessagesController extends Controller
+final class MessagesController extends Controller
 {
-    //
+    public function index()
+    {
+        //todo:
+    }
+
+    public function store()
+    {
+        //todo:
+    }
 }
