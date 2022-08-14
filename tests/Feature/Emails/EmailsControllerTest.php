3ce7a67ebd7352229c8a25d654fb68e8c18806bc
mijael <mijaelsaban@gmail.com>
2022-08-14 10:23:36 +0200

    wip simple index test

--- tests/Feature/Emails/EmailsControllerTest.php
+++ tests/Feature/Emails/EmailsControllerTest.php
@@ -1,6 +1,38 @@
 <?php
 
-final class EmailsControllerTest
-{
+namespace Tests\Feature\Emails;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+final class EmailsControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_index_unauth()
+    {
+        $response = $this->json('get', 'api/emails',
+            [],
+            []
+         );
+
+        $response->assertUnauthorized();
+    }
+
+    public function test_index_auth()
+    {
+        $user = User::factory()->create([
+            'email_verified_at' => null,
+        ]);
+
+        $token = $user->createToken('app-token')->plainTextToken;
+
+        $response = $this->json('get', 'api/emails',
+            [],
+            ['Authorization' => "Bearer " . $token]
+        );
+
+        $response->assertOk();
+    }
 }
